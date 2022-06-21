fdc9872f73a35372b5ea187d959f86c34307ce3f
Bruno Lima <brunoadl88@gmail.com>
2022-06-21 20:53:23 -0300

    drag n drop feature

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+// Fazer uma funcionalidade de contentEditable
+// Fazer uma funcionalidade de Drag n Drop ... FEITO
+// Fazer com que as tarefas sejam salvas
+
 function addTask(e) {
-  const divPai = e.parentElement
-  const ul = divPai.children[1].children[0]
+  const ul = e.previousElementSibling
   const task = cardCreator()
 
-  ul.appendChild(task)
+  ul.innerHTML += task
+  localStorage.setItem(task.id, task)
+  console.log(task)
 }
 
 function cardCreator() {
   const text = prompt("Digite a tarefa")
   let task
 
-  if (text) {
-    task = document.createElement("li")
-    task.setAttribute('draggable', 'true')
-    task.innerHTML = `
+  if (text != "") {
+    task = `
+              <li id="${Date.now()}" ondragstart="drag(event)" draggable="true">
                 <p>${text}</>
                 <p class="remove" onclick="remove(this)">🗑️</p>
+              </li>
             `
   }
 
@@ -26,5 +31,25 @@ function remove(event) {
   const taskToDelete = event.parentElement
   const elementoPai = taskToDelete.parentElement
 
+  console.log(taskToDelete.id)
+
   elementoPai.removeChild(taskToDelete)
+  localStorage.removeItem(taskToDelete.id)
+}
+
+function drag(event) {
+  event.dataTransfer.setData("card", event.target.id)
+}
+
+function drop(event, id) {
+  event.preventDefault()
+  const target = document.getElementById(id)
+  const data = event.dataTransfer.getData("card")
+  const card = document.getElementById(data)
+  target.appendChild(card)
+  event.dataTransfer.clearData()
+}
+
+function over(event) {
+  event.preventDefault()
 }
